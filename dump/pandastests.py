8e281bd90fbccc4e9c1cfbf19ea6dc094c88ba46
Wessel33 <wesleyfrost@me.com>
2018-05-14 19:02:45 +0100

    Update pandastests.py

--- dump/pandastests.py
+++ dump/pandastests.py
@@ -9,3 +9,7 @@ df_list = pd.read_html(html)
 df = df_list[-1]
 print (df)
 df.to_csv('my data.csv')
+
+
+# to take data: 
+df.get_value(5,3,takeable=True) # (row index, col index, default=False (want true) )
