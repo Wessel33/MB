8c914d3c9845eca623a3cc4bb947427cdcea32d8
Wessel33 <wesleyfrost@me.com>
2018-05-14 19:06:02 +0100

    Update pandastests.py

--- dump/pandastests.py
+++ dump/pandastests.py
@@ -2,13 +2,14 @@
 
 import requests
 import pandas as pd
+filenumber = 1
 
 url = 'https://www.bonusbagging.co.uk/oddsmatching.php'
 html = requests.get(url).content
 df_list = pd.read_html(html)
 df = df_list[-1]
 print (df)
-df.to_csv('my data.csv')
+df.to_csv("MB_" + string(filenumber)  + ".csv")
 
 
 # to take data: 
