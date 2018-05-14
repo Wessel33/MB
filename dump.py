71af679c563b11554388f3c718516bfad3878098
Wessel33 <wesleyfrost@me.com>
2018-05-14 13:26:15 +0100

    Create dump.py

---
+++ dump.py
@@ -0,0 +1,7 @@
+from boilerpipe.extract import Extractor
+
+URL='http://radar.oreilly.com/2010/07/louvre-industrial-age-henry-ford.html'
+
+extractor = Extractor(extractor='ArticleExtractor', url=URL)
+
+print extractor.getText()
